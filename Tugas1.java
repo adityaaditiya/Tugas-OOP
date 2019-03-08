cee656ef1a524e71a38c57dac6e0ff0ff0616d8f
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2019-03-08 14:57:54 +0700

    Create Tugas1.java

---
+++ Tugas1.java
@@ -0,0 +1,8 @@
+public class BAditya {
+  public static void main(string[] args){
+    System.out.println("Nama  : Muhammad Aditya");
+    System.out.println("NIM   : 17090152");
+    System.out.println("Kelas : 4B");
+    }
+ }
+ 
